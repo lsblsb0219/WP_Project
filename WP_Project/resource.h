114dfd4874384714e7d10fca771c9f62cecc1c05
이언 박 <yieon1028@gmail.com>
2024-05-27 13:52:54 +0900

    1

---
+++ WP_Project/resource.h
@@ -0,0 +1,19 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++에서 생성한 포함 파일입니다.
+// WP_Project.rc에서 사용되고 있습니다.
+//
+#define ID_VIPfloor                     2
+#define ID_Roomfloor                    103
+#define ID_Normalfloor                  104
+#define ID_Restaurant                   65535
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        105
+#define _APS_NEXT_COMMAND_VALUE         40001
+#define _APS_NEXT_CONTROL_VALUE         1001
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
